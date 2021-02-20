af2367ac07d9a2128cc5a00b55e611b82ae36586
AugeyT <71095950+AugeyT@users.noreply.github.com>
2021-02-19 23:47:51 -0800

    Created the base project
    - Contains Fireside Interactive Studio splash screen
    - Starts on the start of a story pan screen. Use the mouse and scroll wheel to move the camera. Note currently only one way for first picture as once its gone it won't come back.

---
+++ Assets/Scripts/SceneCameraZoom/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 2f;
+    public float minY = 20f;
+    public float maxY = 120f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.y >= Screen.width - panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.z += scroll * scrollSpeed * 20f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+        pos.z = Mathf.Clamp(pos.z, minY, maxY);
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/SceneCameraZoom/UIZoomImageFront.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIZoomImageFront : MonoBehaviour, IScrollHandler
+{
+    private Vector3 initalScale;
+
+    public float ZoomSpeed = 0.1f;
+
+    public float maxZoom = 10f;
+
+    public Image BackImage;
+
+    private UIZoomImage BackProperties;
+
+    private void Awake()
+    {
+        initalScale = transform.localScale;
+        BackProperties = BackImage.GetComponent<UIZoomImage>();
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        var delta = Vector3.one * (eventData.scrollDelta.y * ZoomSpeed);
+        var desiredScale = transform.localScale + delta;
+
+        desiredScale = ClampDesiredScale(desiredScale);
+
+        transform.localScale = desiredScale;
+        if (transform.localScale.z < 2.4f)
+        {
+            BackImage.SendMessage("OnScroll", eventData, SendMessageOptions.DontRequireReceiver);
+        }
+        else
+        {
+            BackProperties.ZoomSpeed = 0.01f;
+            gameObject.GetComponent<Image>().raycastTarget = false;
+        }
+    }
+
+    private Vector3 ClampDesiredScale(Vector3 desiredScale)
+    {
+        desiredScale = Vector3.Max(initalScale, desiredScale);
+        desiredScale = Vector3.Min(initalScale * maxZoom, desiredScale);
+        
+        return desiredScale;
+    }
+}
