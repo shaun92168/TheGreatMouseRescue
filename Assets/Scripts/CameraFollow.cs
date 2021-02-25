47585d2bf03d717891d22929608c1e7cff4efb7b
Jaedin D <jaedindhatt@gmail.com>
2021-02-24 18:56:02 -0800

    basic camera + movement scripts for testing collision
    created simple house layout with assets in living room

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetToFollow;
+    public float smoothSpeed = 0.125f;
+    public Vector3 cameraOffset = new Vector3(0.0f, 15.0f, -0.25f);
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = targetToFollow.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition; 
+    }
+}
