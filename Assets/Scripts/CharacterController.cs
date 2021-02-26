ef145a44aa101d44510bf6022465690892081ee1
John Dong <mahoushisaber@gmail.com>
2021-02-25 20:26:58 -0800

    Update a controllable speed for the player

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,18 +7,21 @@ public class CharacterController : MonoBehaviour
     // public Material[] materials;
     public Renderer rend;
     public Transform trans;
-    public bool isPressed;
+    private bool isPressed;
     // 0 = Stand, 1 = Run , 2 = Sneak, 3 = Crawl
     public int playerState = 0;
-    public float sneakHeight = 0.55f;
-    public float crawlHeight = 0.2f;
+    //public float sneakHeight = 0.55f;
+    //public float crawlHeight = 0.2f;
     public GameState gameState;
 
     Rigidbody rigidBody;
     float hori;
     float vert;
 
-    public float speed = 10.0f;
+    public float speed;
+    public float runSpeed;
+    public float sneakSpeed;
+    public float crawlSpeed;
 
     void Start()
     {
@@ -31,27 +34,26 @@ public class CharacterController : MonoBehaviour
         if (playerState == 0)
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
-            speed = 10.0f;
             rend.GetComponent<Renderer>().material.color = Color.white;
         }
 
         if (playerState == 1)
         {
             transform.localRotation = Quaternion.Euler(80, 0, 0);
-            speed = 15.0f;
+            speed = runSpeed;
             rend.GetComponent<Renderer>().material.color = Color.red;
         }
         if (playerState == 2)
         {
             transform.localRotation = Quaternion.Euler(10, 0, 0);
-            speed = 7.0f;
+            speed = sneakSpeed;
             rend.GetComponent<Renderer>().material.color = Color.yellow;
         }
         if (playerState == 3)
         {
 
             transform.localRotation = Quaternion.Euler(30, 0, 0);
-            speed = 3.0f;
+            speed = crawlSpeed;
             rend.GetComponent<Renderer>().material.color = Color.green;
         }
     }
@@ -75,7 +77,6 @@ public class CharacterController : MonoBehaviour
             {
                 playerState = 1;
             }
-            //playerState = 0;
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
