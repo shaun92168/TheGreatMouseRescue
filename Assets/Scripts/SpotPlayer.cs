a158a6f685af83e3e5dcd921873ccf38d92d1674
Shaun Tseng <shaun92168@gmail.com>
2021-03-19 23:48:12 -0700

    Modify alert system to calculate alert level on each object

--- Assets/Scripts/SpotPlayer.cs
+++ Assets/Scripts/SpotPlayer.cs
@@ -6,17 +6,30 @@ using UnityEngine.UI;
 
 public class SpotPlayer : MonoBehaviour
 {
+    [Header("General")]
+    public Transform player;
+    public GameState gameState;
+
+    // alert system variables
+    [Header("Alert Settings")]
+    public float detectionRange;
+    public bool isPlayerInRange;
+    public float maxAlertLevel = 100;
+    public float alertLevel;
+    public float incrementRate = 20;
+    public float decreasingRate = 5;
+    public float standRate = 1;
+    public float runRate = 1.5f;
+    public float sneakRate = 0.5f;
+    public float crawlRate = 0.2f;
+    private float currentRate;
+
+    //    private Color originalColor;
+    [Header("Others")]
     [SerializeField]
     private Material myMaterial;
     public Color SpotColor;
-    public Transform player;
-    public float detectionRange;
-    public bool isPlayerInRange;
-
-//    private Color originalColor;
     public Text label;
-    public GameState gameState;
-
     public int tempCounter = 0;
 
     void Awake()
@@ -25,11 +38,58 @@ public class SpotPlayer : MonoBehaviour
         myMaterial.color = SpotColor;
     }
 
+    // set up variables
+    void Start()
+    {
+        alertLevel = 0.0f;
+        currentRate = 0.0f;
+    }
+
     void Update()
     {
+        // detect if player is in alert range
         float currentDistanceFromPlayer = Vector3.Distance(player.position, transform.position);
         if (currentDistanceFromPlayer < detectionRange) isPlayerInRange = true;
         else isPlayerInRange = false;
+
+        // set up alert rate based on player movement
+        if (isPlayerInRange)
+        {
+            switch (gameState.playerState)
+            {
+                case 0:
+                    currentRate = standRate;
+                    break;
+                case 1:
+                    currentRate = runRate;
+                    break;
+                case 2:
+                    currentRate = sneakRate;
+                    break;
+                case 3:
+                    currentRate = crawlRate;
+                    break;
+            }
+        }
+
+        // check if alert is full
+        if (alertLevel >= maxAlertLevel)
+        {
+            gameState.isAlertFull = true;
+        }
+
+        // increase or decrease alert base on detecting range
+        if (!gameState.isGameOver)
+        {
+            if (isPlayerInRange)
+            {
+                alertLevel += incrementRate * currentRate * Time.deltaTime;
+            }
+            else if (alertLevel > 0)
+            {
+                alertLevel -= decreasingRate * Time.deltaTime;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
